28e0d6af8879e81caaff16c4024e33e28f06184f
Shaik Rahhil <110285265+skRahil06@users.noreply.github.com>
2023-08-20 13:22:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "shaikrahhil@gmail.com")
+email_entry.insert(0, "@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
